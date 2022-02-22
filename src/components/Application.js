6823ead3b7537bfc76be07070a7d2d62d1038151
willianchu <willianchu@hotmail.com>
2022-02-21 21:39:36 -0800

    inital setup ok

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import "components/Application.scss";
 
@@ -6,7 +6,26 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        {
+          <Fragment>
+            <img
+              className="sidebar--centered"
+              src="images/logo.png"
+              alt="Interview Scheduler"
+            />
+            <hr
+             className="sidebar__separator sidebar--centered"
+            />
+            <nav 
+              className="sidebar__menu">
+            </nav>
+            <img
+              className="sidebar__lhl sidebar--centered"
+              src="images/lhl.png"
+              alt="Lighthouse Labs"
+            />
+            </Fragment>
+          }
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
