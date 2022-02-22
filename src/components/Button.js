52da13aea3696293774d30a4d2fc34f9eb77f72b
willianchu <willianchu@hotmail.com>
2022-02-22 10:10:16 -0800

    button with 3 funnctions

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,23 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = "button";
+ 
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+ 
+   if (props.danger) {
+     buttonClass += " button--danger";
+   }
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
