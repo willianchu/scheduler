e6ed6c49208d25dae0307fd025855a18026dc99f
willianchu <willianchu@hotmail.com>
2022-02-24 19:57:09 -0800

    connected to api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,8 @@
-import React, {Fragment, useState} from "react";
-
+import React, {Fragment, useState, useEffect} from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -67,6 +49,15 @@ const appointments = [
 
 
 export default function Application(props) {
+
+useEffect(() => {
+  axios.get('http://localhost:8001/api/days').then(response => {
+    console.log(response);
+    setDays([...response.data])
+  });
+});
+
+  const [days, setDays] = useState(mockDays);
   
   const [day, setDay] = useState("Monday");
 
