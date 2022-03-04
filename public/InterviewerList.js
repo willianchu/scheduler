46da77ed7cec54db6e27506a3eebf2735d091285
willianchu <willianchu@hotmail.com>
2022-03-03 20:38:10 -0800

    tests initialized

---
+++ public/InterviewerList.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'; 
+
+function InterviewerList(props) {
+  ...
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
